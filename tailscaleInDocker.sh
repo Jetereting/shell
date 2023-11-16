46eecc0333a276342e6595dc86468bab7b7e8d4d
Au <1570967070@qq.com>
2023-11-16 09:27:39 +0800

    Update tailscaleInDocker.sh

--- tailscaleInDocker.sh
+++ tailscaleInDocker.sh
@@ -6,9 +6,9 @@ cp tailscale_1.52.1_amd64/tailscale /usr/bin/tailscale;
 curl -O https://repo.almalinux.org/almalinux/9/BaseOS/x86_64/os/Packages/libmnl-1.0.4-15.el9.x86_64.rpm;
 curl -O https://repo.almalinux.org/almalinux/9/BaseOS/x86_64/os/Packages/libnfnetlink-1.0.1-21.el9.x86_64.rpm;
 curl -O https://repo.almalinux.org/almalinux/9/BaseOS/x86_64/os/Packages/libnetfilter_conntrack-1.0.9-1.el9.x86_64.rpm;
-curl -O https://repo.almalinux.org/almalinux/9/AppStream/x86_64/os/Packages/iptables-devel-1.8.8-6.el9_1.x86_64.rpm;
+curl -O http://mirrors.163.com/centos/7/os/x86_64/Packages/iptables-1.4.21-35.el7.x86_64.rpm;
 rpm -Uvh libmnl-1.0.4-15.el9.x86_64.rpm; 
 rpm -Uvh libnfnetlink-1.0.1-21.el9.x86_64.rpm; 
 rpm -Uvh libnetfilter_conntrack-1.0.9-1.el9.x86_64.rpm; 
-rpm -Uvh iptables-devel-1.8.8-6.el9_1.x86_64.rpm; 
+rpm -Uvh iptables-1.4.21-35.el7.x86_64.rpm; 
 nohup /usr/sbin/tailscaled &
