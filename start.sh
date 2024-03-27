b398b24caafe1227a348ad042b53ee89f5b402f0
Au <1570967070@qq.com>
2024-03-27 13:54:40 +0800

    启动执行目录下的某个应用 例如：cd xxx;start.sh test -c conf/conf.yaml
    启动执行目录下的某个应用 例如：cd xxx;start.sh test -c conf/conf.yaml

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 # 通用启动脚本
-# 启动执行目录下的某个应用 例如：cd xxx;start.sh test '-c conf/conf.yaml'
+# 启动执行目录下的某个应用 例如：cd xxx;start.sh test -c conf/conf.yaml
 echo "start $@"
 # 脚本目录
 #script_dir=$(cd "$(dirname $0)";pwd)/
 # 执行目录
 script_dir=$(pwd)/
 chmod +x ${script_dir}$1
-nohup ${script_dir}$@ > ${script_dir}${app}.out 2>&1 &
+nohup ${script_dir}$@ > ${script_dir}$1.out 2>&1 &
